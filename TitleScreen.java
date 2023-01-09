071d626f3a3dbad570c7ea8b73166f70f30ad7eb
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2023-01-09 14:03:13 -0500

    school branch
    just in case there is a branch at home that is not pushed to origin

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-
+    Label title = new Label("Jumping Dinosaur" , 60);
+    Label start = new Label("Space to start", 50);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -16,5 +17,14 @@ public class TitleScreen extends World
     public TitleScreen()
     {
         super(600,400,1);
+        addObject(title, 300, 150);
+        addObject(start, 300, 250);
+    }
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("space")){
+            GameWorld game = new GameWorld();
+            Greenfoot.setWorld(game);
+        }
     }
 }
